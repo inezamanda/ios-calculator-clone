db623a4a4826de94a61f22794e65187130e39ee9
Inez Yulia Amanda <inez.amanda16@gmail.com>
2021-09-28 12:00:04 +0800

    able to pass parameter in reusable widget

---
+++ lib/button_of_calculator.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ButtonOfCalculator extends StatelessWidget {
+  // const ButtonOfCalculator({Key? key}) : super(key: key);
+
+  // symbol ? => optional
+  final String text;
+  final Color? color;
+  final EdgeInsets? edgeInsets;
+  final dynamic? shape;
+  const ButtonOfCalculator({
+        required this.text,
+        this.color,
+        this.edgeInsets,
+        this.shape});
+  // {} -> bisa melakukan set sebuah attribute optional atau required
+  // {} -> harus define sama attributenya ketika memanggil kelas ini
+  // {} -> dapat memanggil attribute tidak berurutan
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.only(bottom: 10.0),
+        child: MaterialButton (
+            onPressed: (){
+
+            },
+            child: Text(text,
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+              ),
+            ),
+            color: color ?? Color(0xff333333),
+            padding: edgeInsets ?? EdgeInsets.all(22.0),
+            shape: shape ?? CircleBorder()
+        )
+    );
+  }
+}
+
--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'button_of_calculator.dart';
 
 class IosCalculator extends StatefulWidget {
   const IosCalculator({Key? key}) : super(key: key);
@@ -9,55 +10,7 @@ class IosCalculator extends StatefulWidget {
 
 class _IosCalculatorState extends State<IosCalculator> {
 
-  dynamic text ='0';
-  double numOne = 0;
-  double numTwo = 0;
-
-  dynamic result = '';
-  dynamic finalResult = '';
-  dynamic opr = '';
-  dynamic preOpr = '';
-
-  Widget btn(btnText, Color color) {
-    return Container(
-        padding: EdgeInsets.only(bottom: 10.0),
-        child: MaterialButton (
-            onPressed: (){
-              calculation(btnText);
-            },
-            child: Text(btnText,
-              style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.white,
-              ),
-            ),
-            color: color,
-            padding: EdgeInsets.all(22.0),
-            shape: CircleBorder()
-        )
-    );
-  }
-
-  Widget btnZero(btnText) {
-    return Container(
-      padding: EdgeInsets.only(bottom: 10.0),
-      child: ElevatedButton(
-          onPressed: (){
-            calculation(btnText);
-          },
-          child: Text(btnText,
-              style: TextStyle(
-                  fontSize: 30.0
-              )
-          ),
-          style: ElevatedButton.styleFrom(
-              primary: Color(0xff333333),
-              padding: EdgeInsets.only(left:81, top:20, right:81, bottom:20),
-              shape: StadiumBorder()
-          )
-      ),
-    );
-  }
+  String text = '0';
 
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -86,147 +39,53 @@ class _IosCalculatorState extends State<IosCalculator> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                btn('C', Color(0xffa5a5a5)), // AARRGGBB
-                btn('+/-', Color(0xffa5a5a5)),
-                btn('%', Color(0xffa5a5a5)),
-                btn('/', Colors.orange),
-              ],),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                btn('7', Color(0xff333333)),
-                btn('8', Color(0xff333333)),
-                btn('9', Color(0xff333333)),
-                btn('x', Colors.orange),
+                ButtonOfCalculator(text: 'AC', color: Color(0xffa5a5a5)),
+                ButtonOfCalculator(text: '+/-', color: Color(0xffa5a5a5)),
+                ButtonOfCalculator(text: '%', color: Color(0xffa5a5a5)),
+                ButtonOfCalculator(text: '/', color: Colors.orange)
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                btn('4', Color(0xff333333)),
-                btn('5', Color(0xff333333)),
-                btn('6', Color(0xff333333)),
-                btn('-', Colors.orange),
+                ButtonOfCalculator(text: '7'),
+                ButtonOfCalculator(text: '8'),
+                ButtonOfCalculator(text: '9'),
+                ButtonOfCalculator(text: 'x', color: Colors.orange)
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                btn('1', Color(0xff333333)),
-                btn('2', Color(0xff333333)),
-                btn('3', Color(0xff333333)),
-                btn('+', Colors.orange),
+                ButtonOfCalculator(text: '4'),
+                ButtonOfCalculator(text: '5'),
+                ButtonOfCalculator(text: '6'),
+                ButtonOfCalculator(text: '-', color: Colors.orange)
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                btnZero('0'),
-                btn('.', Color(0xff333333)),
-                btn('=', Colors.orange),
+                ButtonOfCalculator(text: '1'),
+                ButtonOfCalculator(text: '2'),
+                ButtonOfCalculator(text: '3'),
+                ButtonOfCalculator(text: '+', color: Colors.orange)
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ButtonOfCalculator(text: '0',
+                    edgeInsets: EdgeInsets.only(
+                        left: 81, top: 20, right: 81, bottom: 20),
+                    shape: StadiumBorder()),
+                ButtonOfCalculator(text: '.'),
+                ButtonOfCalculator(text: '=', color: Colors.orange)
               ],
             ),
           ],
         ),
       ),
     );
-  }
-
-  void calculation(btnText) {
-    if(btnText  == 'C') {
-      text ='0';
-      numOne = 0;
-      numTwo = 0;
-      result = '';
-      finalResult = '';
-      opr = '';
-      preOpr = '';
-
-    } else if( opr == '=' && btnText == '=') {
-
-      if(preOpr == '+') {
-        finalResult = add();
-      } else if( preOpr == '-') {
-        finalResult = sub();
-      } else if( preOpr == 'x') {
-        finalResult = mul();
-      } else if( preOpr == '/') {
-        finalResult = div();
-      }
-
-    } else if(btnText == '+' || btnText == '-' || btnText == 'x' || btnText == '/' || btnText == '=') {
-
-      if(numOne == 0) {
-        numOne = double.parse(result);
-      } else {
-        numTwo = double.parse(result);
-      }
-
-      if(opr == '+') {
-        finalResult = add();
-      } else if( opr == '-') {
-        finalResult = sub();
-      } else if( opr == 'x') {
-        finalResult = mul();
-      } else if( opr == '/') {
-        finalResult = div();
-      }
-      preOpr = opr;
-      opr = btnText;
-      result = '';
-    }
-    else if(btnText == '%') {
-      result = numOne / 100;
-      finalResult = doesContainDecimal(result);
-    } else if(btnText == '.') {
-      if(!result.toString().contains('.')) {
-        result = result.toString()+'.';
-      }
-      finalResult = result;
-    }
-    else if(btnText == '+/-') {
-      result.toString().startsWith('-') ? result = result.toString().substring(1): result = '-'+result.toString();
-      finalResult = result;
-
-    }
-    else {
-      result = result + btnText;
-      finalResult = result;
-    }
-
-    setState(() {
-      text = finalResult;
-    });
-  }
-
-  String add() {
-    result = (numOne + numTwo).toString();
-    numOne = double.parse(result);
-    return doesContainDecimal(result);
-  }
-
-  String sub() {
-    result = (numOne - numTwo).toString();
-    numOne = double.parse(result);
-    return doesContainDecimal(result);
-  }
-  String mul() {
-    result = (numOne * numTwo).toString();
-    numOne = double.parse(result);
-    return doesContainDecimal(result);
-  }
-  String div() {
-    result = (numOne / numTwo).toString();
-    numOne = double.parse(result);
-    return doesContainDecimal(result);
-  }
-
-  String doesContainDecimal(dynamic result) {
-    if(result.toString().contains('.')) {
-      List<String> splitDecimal = result.toString().split('.');
-      if(!(int.parse(splitDecimal[1]) > 0))
-        return result = splitDecimal[0].toString();
-    }
-    return result;
   }
 }
